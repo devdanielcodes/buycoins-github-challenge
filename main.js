2654abed6713f4d0e339eeed6be36ed780902f25
dan-adekoya <60848550+dan-adekoya@users.noreply.github.com>
2020-11-14 18:32:08 +0100

    debugging...

--- main.js
+++ main.js
@@ -2,22 +2,22 @@
 const fetch = require('node-fetch');
 
 const data = {
-    token: '2477623315be1cf8495d816a0edeb3d70c979fb0',
-    username: 'devdanielcodes'
+    "token": "dade8c42e3a99c1a273b8235276131343076c3d3",
+    "username": "devdanielcodes"
 }
 
 const body = {
-    'query': `
+    "query": `
 
     query { 
-        user(login: ${data.username}){
+        user(login: "${data.username}"){
           name
           avatarUrl
           repositories(last: 20){
             nodes{
               name
               primaryLanguage {
-                id
+                color
                 name
               }
               stargazerCount
@@ -31,25 +31,23 @@ const body = {
             `
 }
 
-const headers = {
-    'Content-Type': "application/json",
-    Authentication: `bearer ${data.token}`
+const header = {
+    "Content-Type": "application/json",
+    "Authentication": "bearer "+ data.token
 }
-const myData = []
+let myData = {}
 
 fetch('https://api.github.com/graphql', {
     method: "POST",
-    headers: headers,
+    headers: header,
     body: JSON.stringify(body)
 })
-.then(response => response.json())
-.then(data => {
+.then((response) => console.log(response))
+/* .then((data) => {
 
     console.log(data)
    
-   /*  console.log(myData.push({
-        name: data.data().name,
-        //language: data.data.repositories.nodes
-    })) */
-})
-.catch(error => console.log(JSON.stringify(error)))
+      myData = data
+      console.log(myData)
+}) */
+.catch((error) => console.log(JSON.stringify(error)))
